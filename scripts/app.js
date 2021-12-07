751507648d02f5ccbd507f49f8fce16e5c6a7a4e
Tanous Atallah <tanous.atallah@gmail.com>
2021-12-07 17:06:08 +0100

    modal err style+validation  js

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,18 @@
-const btnEditPlayer1 = document.getElementById("edit-player1-btn");
-const btnEditPlayer2 = document.getElementById("edit-player2-btn");
 
 const playerConfigOverlay = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
 
+const btnEditPlayer1 = document.getElementById("edit-player1-btn");
+const btnEditPlayer2 = document.getElementById("edit-player2-btn");
 const btnCancelConfig = document.getElementById("cancel-config-btn");
 
 btnEditPlayer1.addEventListener("click", openPlayerConfig );
 btnEditPlayer2.addEventListener("click", openPlayerConfig );
 
 btnCancelConfig.addEventListener("click", closePlayerConfig );
-
 backdropElement.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig);
+
--- scripts/config.js
+++ scripts/config.js
@@ -5,5 +5,21 @@ function openPlayerConfig(){
 
 function closePlayerConfig() {
     playerConfigOverlay.style.display = "none";
-    backdropElement.style.display = "none";  
+    backdropElement.style.display = "none"; 
+    formElement.firstElementChild.classList.remove("error");
+    errorsOutputElement.textContent ="";
+}
+
+function savePlayerConfig(event) {
+    event.preventDefault(); // prevent the default form behaviour form happening
+    // that we won't browser-reload
+    const formData = new FormData(event.target); // extract the input fron the form automaticaly
+    // FormData object, extract all the input-values from the form, but the input field should have a 'name' attribut
+    const enteredPlayername = formData.get('playername').trim();
+    if (!enteredPlayername) {
+        event.target.firstElementChild.classList.add("error");
+        errorsOutputElement.textContent = "Please enter a valid name!";
+        // return;
+    }
+
 }
