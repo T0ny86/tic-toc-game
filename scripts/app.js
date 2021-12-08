b721d694737dfcec05ebeebb1020ab98eaa5049c
Tanous Atallah <tanous.atallah@gmail.com>
2021-12-08 14:11:20 +0100

    switch players + click events logic added

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 let editedPlayer = 0;
-
+let activePlayer = 0;
 const players = [
     {
         name:'',
@@ -16,7 +16,7 @@ const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors");
 const gameAreaElement = document.getElementById("active-game");
-
+const activePlayerNameElement = document.getElementById("active-player-name");
 
 const btnEditPlayer1 = document.getElementById("edit-player1-btn");
 const btnEditPlayer2 = document.getElementById("edit-player2-btn");
@@ -32,3 +32,24 @@ backdropElement.addEventListener("click", closePlayerConfig);
 formElement.addEventListener("submit", savePlayerConfig);
 
 btnNewGame.addEventListener("click", startNewGame);
+
+
+/*
+// return all the li elements in ol as array
+const gameFieldElements = document.querySelectorAll("#game-board li");
+for(const gameFieldElement of gameFieldElements){
+    gameAreaElement.addEventListener("click", selectGameField);
+}
+*/
+// another method to add event listner to all ol/ul list items
+// it is shorter, but had side effect,
+//that is when player click on gap between li items, it will be clickable too
+// so we should ad some filter or avoid it, in callback function
+const gameBoardElement = document.getElementById("game-board");
+gameBoardElement.addEventListener("click", selectGameField);
+
+/*
+both above approach are work well , 
+1- adding eventListener to each item we want it clickable
+2- adding eventListener to parent element, and handeling the side-effect in the callback function later
+*/
--- scripts/game.js
+++ scripts/game.js
@@ -3,5 +3,27 @@ function startNewGame(){
     alert("Please set custom player names for both players!");
     return; // to exit this function if the condition was true 
    }
+
+   activePlayerNameElement.textContent = players[activePlayer].name;
    gameAreaElement.style.display= "block";
+}
+
+function switchPlayer() {
+    if (activePlayer === 0) {
+        activePlayer = 1;
+    }else{
+        activePlayer = 0;
+    }
+    activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event){
+    if (event.target.tagName !== 'LI') {
+        // to avoid random clicks on gaps between li items
+        // so only wanted clicks will pass this if statment
+        return;
+    }
+    event.target.textContent = players[activePlayer].symbol;
+    event.target.classList.add("disabled");
+    switchPlayer();
 }
